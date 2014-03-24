07d82d583cf816ce76644aa3c2a2c948171ebd9b
Jaime Borschuk <jaime.borschuk@gmail.com>
2014-03-23 17:45:06 -0700

    mods

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/pop', function(req, res){
+app.get('/', function(req, res){
   res.send('pop blog');
+});
+
+app.get('/admin', function(req, res){
+  res.send('pop blog admin');
 });
 
 var server = app.listen(3000, function() {
